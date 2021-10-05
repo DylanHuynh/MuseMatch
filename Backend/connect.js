e33bf87e5ab4b74e9633360c8653e4b46f4b1761
Vaibhav Mohata <vaibhav.mohata@berkeley.edu>
2021-10-05 14:35:36 -0700

    Basic setup code for writing and reading from MongoDB

--- Backend/connect.js
+++ Backend/connect.js
@@ -2,49 +2,41 @@ const { MongoClient } = require('mongodb');
 
 // Replace the following with your Atlas connection string
 const url = 'mongodb+srv://MuseMatch:musematch123@cluster0.6ip8o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-//const client = new MongoClient(url);
+const client = new MongoClient(url);
 
+async function write(userInfo) {
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("beta");
+        dbo.collection("user").insertOne(userInfo, function(err, res) {
+          if (err) throw err;
+          console.log("1 document inserted");
+          db.close();
+        });
+      });
+}
 
-// async function listDatabases(client){
-//     databasesList = await client.db().admin().listDatabases();
- 
-//     console.log("Databases:");
-//     databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-// };
+async function read(id) {
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("beta");
+        dbo.collection("user").findOne({
+            id: id
+        }, function(err, result) {
+          if (err) throw err;
+          console.log(result.name, result.genres);
+          db.close();
+        });
+      });
+}
 
+write(
+    {
+        name: "Veronica",
+        genres: "Rock",
+        location: "Berkeley",
+        id: "3"
+    }
+)
 
-// async function run() {
-//     try {
-//         await client.connect();
-//         console.log("Connected correctly to server");
-//         await listDatabases(client);
-//     } catch (err) {
-//         console.log(err.stack);
-//     }
-//     finally {
-//         await client.close();
-//     }
-// };
-
-// run().catch(console.dir);
-
-// MongoClient.connect(url, function(err, db) {
-//     if (err) throw err;
-//     var dbo = db.db("beta");
-//     var firstUser = { name: "John Smith", genres: "Rock, Pop, Country"};
-//     dbo.collection("user").insertOne(firstUser, function(err, res) {
-//       if (err) throw err;
-//       console.log("1 document inserted");
-//       db.close();
-//     });
-//   });
-
-  MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db("beta");
-    dbo.collection("user").findOne({}, function(err, result) {
-      if (err) throw err;
-      console.log(result.name, result.genres);
-      db.close();
-    });
-  });
+console.log(read("3"));
