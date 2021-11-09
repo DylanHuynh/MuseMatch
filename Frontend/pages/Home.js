39a24ed4731fa67a261c70e188520e001694b30b
Dylan Huynh <dyhuynh@berkeley.edu>
2021-11-09 12:44:00 -0800

    added ionics icons and fixed header showing problem

--- Frontend/pages/Home.js
+++ Frontend/pages/Home.js
@@ -3,7 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useFonts } from 'expo-font';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
+import { Ionicons } from '@expo/vector-icons';
 import Homepage from '../pages/Homepage';
 import SwipeView from '../pages/SwipeView';
 
@@ -13,10 +13,29 @@ const Stack = createNativeStackNavigator();
 
 const H = () => {
     return (
-        <NavigationContainer independent = {true}>
-            <Tab.Navigator>
+        <NavigationContainer independent={true}>
+            <Tab.Navigator
+                screenOptions={({ route }) => ({
+                    headerShown: false,
+                    tabBarIcon: ({ color, size }) => {
+                        let iconName;
+
+                        if (route.name === 'Homepage') {
+                            iconName = 'home-outline'
+
+                        } else if (route.name === 'SwipeView') {
+                            iconName = 'person-add-outline'
+                        }
+
+                        // You can return any component that you like here!
+                        return <Ionicons name={iconName} size={size} color={color} />;
+                    },
+                    tabBarActiveTintColor: 'tomato',
+                    tabBarInactiveTintColor: 'gray',
+                })}
+            >
                 <Stack.Screen name="Homepage" component={Homepage} />
-                <Stack.Screen name="SwipeView" component={SwipeView}/>
+                <Stack.Screen name="SwipeView" component={SwipeView} />
             </Tab.Navigator>
         </NavigationContainer>
     )
