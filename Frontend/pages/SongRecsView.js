ef872cb5f15c32f4f0ca0b735e65a26f8b3752bf
Shay Ma <shayma@berkeley.edu>
2021-12-05 21:32:10 -0800

    Working like song button

--- Frontend/pages/SongRecsView.js
+++ Frontend/pages/SongRecsView.js
@@ -12,46 +12,27 @@ import * as SecureStore from 'expo-secure-store';
 
 
 const SongRecsView = ({ navigation }) => {
-  const [isHeart, setIsHeart] = useState(false);
-  // const [data, setData] = useState(testData);
+  
   const arr = Array(data.length);
   for (let i = 0; i < data.length; i++) {
     arr[i] = i;
   }
-  // const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   console.log(data[0].song_name);
 
   
-// const heartPress = (props) => {
-//   console.log("heart #" + props.number + " pressed")
-//   setIsHeart(!isHeart)
-// }
+
 
   return (
     <View style = {styles.card}>
       <Text style = {styles.title}>Explore Your Curated Recs</Text>
+      
 
       <View style = {{height: 300}}>
         <ScrollView style = {styles.scrollView}>
             <TouchableOpacity activeOpacity={1}>
                 {arr.map((number) => {
                     return (
-                      <View>
-                        <View style = {styles.row}>
-                          <Image style = {styles.topSongsCellImage} source={{ uri: data[number].album_cover }}/>
-                          <View style = {{marginLeft: 10}}>
-                            <Text style = {styles.topSongsTitle}>{data[number].song_name}</Text>
-                            <Text style = {styles.topSongsArtist}>{data[number].artist}</Text>
-                          </View>
-                        </View>
-                        <View style = {{flexDirection: 'column', paddingRight: 40, position: 'absolute', left: 240, top: 20}}>
-                          <TouchableOpacity onPress={() => console.log("heart #" + number + " pressed")}>
-                            <View style={{alignItems: 'flex-end', marginLeft: 40}}>
-                              <Image style={[{tintColor: isHeart ? 'red' : 'black'}, styles.circleHeartImage]} source={require('../assets/heart-icon.png')} />
-                            </View>
-                          </TouchableOpacity>
-                        </View>
-                      </View>                      
+                      <RowView number={number}/>                      
                     )
                 })}
             </TouchableOpacity>
@@ -62,7 +43,31 @@ const SongRecsView = ({ navigation }) => {
   )
 }
 
-
+const RowView = (props) => {
+  const [isHeart, setIsHeart] = useState(false);
+  const heartPress = () => {
+    console.log("heart #" + props.number + " pressed")
+    setIsHeart(!isHeart)
+  }
+  return (
+    <View>
+      <View style = {styles.row}>
+        <Image style = {styles.topSongsCellImage} source={{ uri: data[props.number].album_cover }}/>
+        <View style = {{marginLeft: 10}}>
+          <Text style = {styles.topSongsTitle}>{data[props.number].song_name}</Text>
+          <Text style = {styles.topSongsArtist}>{data[props.number].artist}</Text>
+        </View>
+      </View>
+      <View style = {{flexDirection: 'column', paddingRight: 40, position: 'absolute', left: 240, top: 20}}>
+        <TouchableOpacity onPress={() => heartPress() }>
+          <View style={{alignItems: 'flex-end', marginLeft: 40}}>
+            <Image id={props.number} style={[{tintColor: isHeart ? 'red' : 'black'}, styles.circleHeartImage]} source={require('../assets/heart-icon.png')} />
+          </View>
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
 
 
 export default SongRecsView
